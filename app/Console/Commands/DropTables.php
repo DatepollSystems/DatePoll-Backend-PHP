48d88782703938451a6b4224fbaa9e1afaea7bef
Dominik <git@dafnik.me>
2019-01-30 08:04:04 +0100

    Fix gitlab ci

---
+++ app/Console/Commands/DropTables.php
@@ -0,0 +1,44 @@
+<?php namespace App\Console\Commands;
+
+use Schema;
+use Illuminate\Console\Command;
+
+class DropTables extends Command
+{
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'droptables';
+
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Command description';
+
+  /**
+   * Create a new command instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  /**
+   * Execute the console command.
+   *
+   * @return mixed
+   */
+  public function handle()
+  {
+    \Schema::dropAllTables();
+
+    $this->comment(PHP_EOL."If no errors showed up, all tables were dropped".PHP_EOL);
+
+  }
+}
