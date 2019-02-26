8812bc474d0d79ebdde2c56f77830dc68e82962c
Dominik <git@dafnik.me>
2019-02-26 10:45:46 +0100

    Create models

---
+++ app/Models/Groups/Group.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models\Groups;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property string $created_at
+ * @property string $updated_at
+ * @property Subgroup[] $subgroups
+ * @property UsersMemberOfGroups[] $usersMemberOfGroups
+ */
+class Group extends Model
+{
+  /**
+   * @var array
+   */
+  protected $fillable = ['name', 'description', 'created_at', 'updated_at'];
+
+  /**
+   * @return \Illuminate\Database\Eloquent\Collection
+   */
+  public function subgroups() {
+    return $this->hasMany('App\Subgroup')->get();
+  }
+
+  /**
+   * @return \Illuminate\Database\Eloquent\Collection
+   */
+  public function usersMemberOfGroups() {
+    return $this->hasMany('App\UsersMemberOfGroups')->get();
+  }
+}
---
+++ app/Models/Subgroups/UsersMemberOfSubgroups.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models\Subgroups;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property int $subgroup_role_id
+ * @property int $subgroup_id
+ * @property int $user_id
+ * @property string $created_at
+ * @property string $updated_at
+ * @property Subgroup $subgroup
+ * @property SubgroupRole $subgroupRole
+ * @property User $user
+ */
+class UsersMemberOfSubgroups extends Model
+{
+  /**
+   * @var array
+   */
+  protected $fillable = ['subgroup_role_id', 'subgroup_id', 'user_id', 'created_at', 'updated_at'];
+
+  /**
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+   */
+  public function subgroup() {
+    return $this->belongsTo('App\Subgroup')->first();
+  }
+
+  /**
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+   */
+  public function subgroupRole() {
+    return $this->belongsTo('App\SubgroupRole')->first();
+  }
+
+  /**
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+   */
+  public function user() {
+    return $this->belongsTo('App\User')->first();
+  }
+}
