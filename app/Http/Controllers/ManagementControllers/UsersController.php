e8eeb03d2dfe037f6e2ca686920c9d40a9791244
Dominik <git@dafnik.me>
2019-05-15 13:59:21 +0200

    Add permissions

--- app/Http/Controllers/ManagementControllers/UsersController.php
+++ app/Http/Controllers/ManagementControllers/UsersController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers\ManagementControllers;
 use App\Http\Controllers\Controller;
 use App\Mail\ActivateUser;
 use App\Models\User\User;
-use App\Models\UserCode;
+use App\Models\User\UserPermission;
 use App\Models\User\UserTelephoneNumber;
+use App\Models\UserCode;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Mail;
@@ -44,7 +45,16 @@ class UsersController extends Controller
       $toReturnUser->activated = $user->activated;
       $toReturnUser->activity = $user->activity;
       $toReturnUser->phoneNumbers = $user->telephoneNumbers();
-      $toReturnUser->permissions = $user->permissions();
+
+      $permissions = array();
+      if($user->permissions() != null) {
+        foreach ($user->permissions() as $permission) {
+          $permissions[] = $permission->permission;
+        }
+      }
+
+      $toReturnUser->permissions = $permissions;
+      $toReturnUser->performanceBadges = $user->performanceBadges();
 
       $toReturnUser->view_user = ['href' => 'api/v1/management/users/' . $user->id, 'method' => 'GET'];
 
@@ -64,19 +74,47 @@ class UsersController extends Controller
    * @throws ValidationException
    */
   public function create(Request $request) {
-    $this->validate($request, ['title' => 'max:190', 'email' => 'required|email', 'firstname' => 'required|max:190|min:1', 'surname' => 'required|max:190|min:1', 'birthday' => 'required|date', 'join_date' => 'required|date', 'streetname' => 'required|max:190|min:1', 'streetnumber' => 'required|max:190|min:1', 'zipcode' => 'required|integer', 'location' => 'required|max:190|min:1', 'activated' => 'required|boolean', 'activity' => 'required|max:190|min:1', 'phoneNumbers' => 'array']);
+    $this->validate($request, [
+      'title' => 'max:190',
+      'email' => 'required|email',
+      'firstname' => 'required|max:190|min:1',
+      'surname' => 'required|max:190|min:1',
+      'birthday' => 'required|date',
+      'join_date' => 'required|date',
+      'streetname' => 'required|max:190|min:1',
+      'streetnumber' => 'required|max:190|min:1',
+      'zipcode' => 'required|integer',
+      'location' => 'required|max:190|min:1',
+      'activated' => 'required|boolean',
+      'activity' => 'required|max:190|min:1',
+      'phoneNumbers' => 'array',
+      'permissions' => 'array']);
 
     $email = $request->input('email');
     $firstname = $request->input('firstname');
     $surname = $request->input('surname');
     $activated = $request->input('activated');
     $phoneNumbers = $request->input('phoneNumbers');
+    $permissions = $request->input('permissions');
 
     if (User::where('email', $email)->first() != null) {
       return response()->json(['msg' => 'The email address is already used', 'error_code' => 'email_address_already_used'], 400);
     }
 
-    $user = new User(['title' => $request->input('title'), 'email' => $email, 'firstname' => $firstname, 'surname' => $surname, 'birthday' => $request->input('birthday'), 'join_date' => $request->input('join_date'), 'streetname' => $request->input('streetname'), 'streetnumber' => $request->input('streetnumber'), 'zipcode' => $request->input('zipcode'), 'location' => $request->input('location'), 'activated' => $activated, 'activity' => $request->input('activity'), 'password' => 'Null']);
+    $user = new User([
+      'title' => $request->input('title'),
+      'email' => $email,
+      'firstname' => $firstname,
+      'surname' => $surname,
+      'birthday' => $request->input('birthday'),
+      'join_date' => $request->input('join_date'),
+      'streetname' => $request->input('streetname'),
+      'streetnumber' => $request->input('streetnumber'),
+      'zipcode' => $request->input('zipcode'),
+      'location' => $request->input('location'),
+      'activated' => $activated,
+      'activity' => $request->input('activity'),
+      'password' => 'Null']);
 
     if (!$user->save()) {
       return response()->json(['msg' => 'An error occurred during user saving..'], 500);
@@ -87,6 +125,13 @@ class UsersController extends Controller
         $phoneNumberToSave = new UserTelephoneNumber(['label' => $phoneNumber['label'], 'number' => $phoneNumber['number'], 'user_id' => $user->id]);
 
         $phoneNumberToSave->save();
+      }
+    }
+
+    if($permissions != null) {
+      foreach ((array)$permissions as $permission) {
+        $permissionToSave = new UserPermission(['permission' => $permission, 'user_id' => $user->id]);
+        $permissionToSave->save();
       }
     }
 
@@ -102,6 +147,7 @@ class UsersController extends Controller
     $user = User::find($user->id);
 
     $userToShow = new stdClass();
+    $userToShow->id = $user->id;
     $userToShow->title = $user->title;
     $userToShow->email = $user->email;
     $userToShow->firstname = $user->firstname;
@@ -116,7 +162,16 @@ class UsersController extends Controller
     $userToShow->activity = $user->activity;
     $userToShow->force_password_change = $user->force_password_change;
     $userToShow->phoneNumbers = $user->telephoneNumbers();
-    $userToShow->permissions = $user->permissions();
+
+    $permissions = array();
+    if($user->permissions() != null) {
+      foreach ($user->permissions() as $permission) {
+        $permissions[] = $permission->permission;
+      }
+    }
+
+    $userToShow->permissions = $permissions;
+
     $userToShow->performanceBadges = $user->performanceBadges();
     $userToShow->view_user = ['href' => 'api/v1/management/users/' . $user->id, 'method' => 'GET'];
 
@@ -153,7 +208,16 @@ class UsersController extends Controller
     $userToShow->activity = $user->activity;
     $userToShow->force_password_change = $user->force_password_change;
     $userToShow->phoneNumbers = $user->telephoneNumbers();
-    $userToShow->permissions = $user->permissions();
+
+    $permissions = array();
+    if($user->permissions() != null) {
+      foreach ($user->permissions() as $permission) {
+        $permissions[] = $permission->permission;
+      }
+    }
+
+    $userToShow->permissions = $permissions;
+    $userToShow->performanceBadges = $user->performanceBadges();
 
     $userToShow->view_users = ['href' => 'api/v1/management/users', 'method' => 'GET'];
 
@@ -170,7 +234,21 @@ class UsersController extends Controller
    * @throws ValidationException
    */
   public function update(Request $request, $id) {
-    $this->validate($request, ['title' => 'max:190', 'email' => 'required|email', 'firstname' => 'required|max:190|min:1', 'surname' => 'required|max:190|min:1', 'birthday' => 'required|date', 'join_date' => 'required|date', 'streetname' => 'required|max:190|min:1', 'streetnumber' => 'required|max:190|min:1', 'zipcode' => 'required|integer', 'location' => 'required|max:190|min:1', 'activated' => 'required|boolean', 'activity' => 'required|max:190|min:1', 'phoneNumbers' => 'array']);
+    $this->validate($request, [
+      'title' => 'max:190',
+      'email' => 'required|email',
+      'firstname' => 'required|max:190|min:1',
+      'surname' => 'required|max:190|min:1',
+      'birthday' => 'required|date',
+      'join_date' => 'required|date',
+      'streetname' => 'required|max:190|min:1',
+      'streetnumber' => 'required|max:190|min:1',
+      'zipcode' => 'required|integer',
+      'location' => 'required|max:190|min:1',
+      'activated' => 'required|boolean',
+      'activity' => 'required|max:190|min:1',
+      'phoneNumbers' => 'array',
+      'permissions' => 'array']);
 
     $user = User::find($id);
     if ($user == null) {
@@ -189,6 +267,7 @@ class UsersController extends Controller
     $surname = $request->input('surname');
     $activated = $request->input('activated');
     $phoneNumbers = $request->input('phoneNumbers');
+    $permissions = $request->input('permissions');
 
     $user->email = $email;
     $user->title = $request->input('title');
@@ -248,6 +327,45 @@ class UsersController extends Controller
       }
     }
     //---------------------------------------------------------------
+    //---- Permissions manager only deletes changed permissions -----
+    $permissionsWhichHaveNotBeenDeleted = array();
+
+    $OldPermissions = $user->permissions();
+    foreach ($OldPermissions as $oldPermission) {
+      $toDelete = true;
+
+      foreach ((array) $permissions as $permission) {
+        if($oldPermission['permission'] == $permission) {
+          $toDelete = false;
+          $permissionsWhichHaveNotBeenDeleted[] = $permission;
+          break;
+        }
+      }
+
+      if($toDelete) {
+        $permissionToDeleteObject = UserPermission::find($oldPermission->id);
+        if (!$permissionToDeleteObject->delete()) {
+          return response()->json(['msg' => 'Failed during permission clearing...'], 500);
+        }
+      }
+    }
+
+    foreach ((array) $permissions as $permission) {
+      $toAdd = true;
+
+      foreach ($permissionsWhichHaveNotBeenDeleted as $permissionWhichHaveNotBeenDeleted) {
+        if($permission == $permissionWhichHaveNotBeenDeleted) {
+          $toAdd = false;
+          break;
+        }
+      }
+
+      if($toAdd) {
+        $permissionToSave = new UserPermission(['permission' => $permission, 'user_id' => $user->id]);
+        $permissionToSave->save();
+      }
+    }
+    //---------------------------------------------------------------
 
     if ($activated AND !$oldActivatedStatus) {
       $randomPassword = UserCode::generateCode();
@@ -258,7 +376,8 @@ class UsersController extends Controller
       Mail::to($user->email)->send(new ActivateUser($firstname . " " . $surname, $randomPassword));
     }
 
-    $userToShow = new \stdClass();
+    $userToShow = new stdClass();
+    $userToShow->id = $user->id;
     $userToShow->title = $user->title;
     $userToShow->email = $user->email;
     $userToShow->firstname = $user->firstname;
@@ -273,7 +392,15 @@ class UsersController extends Controller
     $userToShow->activity = $user->activity;
     $userToShow->force_password_change = $user->force_password_change;
     $userToShow->phoneNumbers = $user->telephoneNumbers();
-    $userToShow->permissions = $user->permissions();
+
+    $permissions = array();
+    if($user->permissions() != null) {
+      foreach ($user->permissions() as $permission) {
+        $permissions[] = $permission->permission;
+      }
+    }
+
+    $userToShow->performanceBadges = $user->performanceBadges();
     $userToShow->view_user = ['href' => 'api/v1/management/users/' . $user->id, 'method' => 'GET'];
 
     $response = ['msg' => 'User updated', 'user' => $userToShow];
