4c2fc71a1bae7604087b5029c9228bea55535acc
Dominik <git@dafnik.me>
2020-10-08 12:10:13 +0200

    [feat] Add place reservation migrations

---
+++ app/Models/SeatReservation/Place.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models\SeatReservation;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property double $x
+ * @property double $y
+ * @property string $created_at
+ * @property string $updated_at
+ * @property PlaceReservation[] $placeReservations
+ */
+class Place extends Model
+{
+
+  /**
+   * The table associated with the model.
+   *
+   * @var string
+   */
+  protected $table = 'places';
+
+  /**
+   * @var array
+   */
+  protected $fillable = [
+    'name',
+    'x',
+    'y',
+    'created_at',
+    'updated_at'];
+
+
+  /**
+   * @return Collection | PlaceReservation[] | null
+   */
+  public function broadcastsForSubgroups()
+  {
+    return $this->hasMany(PlaceReservation::class)
+      ->get();
+  }
+}
---
+++ database/migrations/2000_01_01_000605_create_places_reservations_by_users_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Models\SeatReservation\PlaceReservationState;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlacesReservationsTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('places_reservations_by_users', function (Blueprint $table) {
+      $table->increments('id');
+
+      $table->string('reason')->nullable(false);
+      $table->string('description')->nullable(true);
+      $table->date('start_date')->nullable(false);
+      $table->date('end_date')->nullable(false);
+      $table->string('state')->nullable(false)->default(PlaceReservationState::WAITING);
+
+      $table->integer('place_id')->unsigned();
+      $table->foreign('place_id')
+          ->references('id')->on('places')->onDelete('cascade');
+
+      $table->integer('user_id')->unsigned()->nullable(true);
+      $table->foreign('user_id')
+          ->references('id')->on('users');
+
+      $table->integer('approver_id')->unsigned()->nullable(true);
+      $table->foreign('approver_id')
+          ->references('id')->on('users');
+
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('places_reservations_by_users');
+  }
+}
