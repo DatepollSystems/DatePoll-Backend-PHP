79c2355376df868b8b6b7b9e9adeb33bf892d051
Dominik <git@dafnik.me>
2020-11-26 20:28:30 +0100

    Bump version to 0.10.0

--- app/Versions.php
+++ app/Versions.php
@@ -3,8 +3,8 @@
 namespace App;
 
 class Versions {
-  private static string $application_version_string = '0.9.1';
-  private static int $application_version = 23;
+  private static string $application_version_string = '0.10.0';
+  private static int $application_version = 24;
 
   private static int $database_version = 7;
 
